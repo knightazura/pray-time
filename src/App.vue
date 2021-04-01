dddfbb22cd6b8496dc5be2ee2a39b4ac6129312a
Muhammad Izzuddin Al Fikri <azura.azureus.ikhie@gmail.com>
2021-04-01 13:22:33 +0700

    Improve layout for landing page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <!-- New Design -->
-  <main class="grid grid-cols-1 px-9 h-screen" style="background-color: #D1E1ED; grid-template-rows: max-content 1fr max-content">
-    <h1 style="line-height: 150%; padding-top: 80px;">
+  <main class="grid full-bleed-grid px-9 min-h-screen" style="background-color: #D1E1ED; grid-template-rows: max-content 1fr max-content">
+    <h1 class="pt-10 md:pt-20" style="line-height: 150%;">
       Know your <br>
       <span class="font-black" style="color: #0099FF">pray times</span> for <br>
       <span class="font-black">today</span>
     </h1>
     <NewSearchInput />
-    <div class="credits place-self-end flex flex-col mb-8">
-      <mzkLogo />
-      <p class="mt-6">Developed by Muhammad Izzuddin Al Fikri</p>
+    <div class="full-bleed credits mb-4">
+      <div class="flex justify-center">
+        <mzkLogo />
+      </div>
+      <p class="text-center mt-4">Developed by Muhammad Izzuddin Al Fikri</p>
     </div>
   </main>
 </template>
 
 <script>
-// Modules
-import { computed, toRefs } from 'vue';
-
-// Local modules
-import { prayTimes, nextActivePray } from './lib/data/pray-time';
-import { places } from './lib/data/places';
-
 // Components
 import NewSearchInput from './components/SearchBox/NewSearchInput.vue';
 import mzkLogo from './components/Elements/mzkLogo.vue';
