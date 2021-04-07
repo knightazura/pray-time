36053546c212d392acf115e1e8b0b8c38cf0147d
Muhammad Izzuddin al Fikri <izzuddin.alfikri@icloud.com>
2021-04-07 20:47:42 +0700

    Use full string to call env vars

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,15 @@
-const GEOCITIES_API = import.meta.env.VITE_GEOCITIES_API;
-const PRAYTIMES_API = import.meta.env.VITE_PRAYTIMES_API;
+const GEOCITIES_API =
+  process.env.NODE_ENV === "production"
+    ? "import.meta\u200b.env.VITE_GEOCITIES_API"
+    : import.meta.env.VITE_GEOCITIES_API;
+const PRAYTIMES_API =
+  process.env.NODE_ENV === "production"
+    ? "import.meta\u200b.env.VITE_PRAYTIMES_API"
+    : import.meta.env.VITE_PRAYTIMES_API;
 
 const SERVICES = {
   GEOCITIES_API,
   PRAYTIMES_API
-}
+};
 
 export default SERVICES;
